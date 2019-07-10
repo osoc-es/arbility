d5a1eccca78c6d3786d650f0cd6e79742bce9c8c
Dani <danisom1b@gmail.com>
2019-07-10 16:38:39 +0200

    estructura Log In General

--- app/src/main/java/com/osoc/oncera/GuestActivity.java
+++ app/src/main/java/com/osoc/oncera/GuestActivity.java
@@ -2,13 +2,28 @@ package com.osoc.oncera;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class GuestActivity extends AppCompatActivity {
-
+    ImageView salir;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guest);
+        salir = (ImageView) findViewById( R.id.BotonSalir);
+        salir.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        } );
+
     }
+
+
+
 }
