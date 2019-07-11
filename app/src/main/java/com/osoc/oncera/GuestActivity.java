d60c5db5d87e9939ce00958e51f222b2985abaeb
Sergio Gavilan <ssjsrgx@gmail.com>
2019-07-11 15:46:06 +0200

    Updated layouts

--- app/src/main/java/com/osoc/oncera/GuestActivity.java
+++ app/src/main/java/com/osoc/oncera/GuestActivity.java
@@ -6,21 +6,26 @@ import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class GuestActivity extends AppCompatActivity {
-    ImageView salir;
+    ImageButton botonSalir;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guest);
-        salir = (ImageView) findViewById( R.id.BotonSalir);
-        salir.setOnClickListener( new View.OnClickListener() {
+
+        botonSalir = (ImageButton)findViewById(R.id.BotonSalir);
+
+        botonSalir.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
                 finish();
             }
-        } );
+        });
 
     }
 
--- app/src/main/java/com/osoc/oncera/MainActivity.java
+++ app/src/main/java/com/osoc/oncera/MainActivity.java
@@ -2,6 +2,7 @@ package com.osoc.oncera;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -10,6 +11,7 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     private Button guestButton;
+    private Button studentButton;
 
 
     @Override
@@ -21,14 +23,24 @@ public class MainActivity extends AppCompatActivity {
 
         guestButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-               changeToGuest();
+                changeWindowTo(GuestActivity.class);
+            }
+        });
+
+        studentButton = (Button)findViewById(R.id.BotonAlumno);
+
+        studentButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                changeWindowTo(LegalInfoActivity.class);
             }
         });
 
     }
 
-    void changeToGuest(){
-        Intent guestActivity = new Intent(this,GuestActivity.class);
+    void changeWindowTo(Class activity){
+        Intent guestActivity = new Intent(this,activity);
         startActivity(guestActivity);
     }
+
+
 }
