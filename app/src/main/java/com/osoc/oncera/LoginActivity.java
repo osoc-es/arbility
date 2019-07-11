c8cf41ca2fa34cd008606c2fd1cfeb902eefd12f
Dani <danisom1b@gmail.com>
2019-07-11 10:59:22 +0200

    estructura Log In General

--- app/src/main/java/com/osoc/oncera/LoginActivity.java
+++ app/src/main/java/com/osoc/oncera/LoginActivity.java
@@ -3,12 +3,28 @@ package com.osoc.oncera;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText correo;
+    private EditText password;
+    private FirebaseAuth auth;
+    private DatabaseReference mDatabaseRef;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_login );
+
+        correo = (EditText) findViewById( R.id.etCorreoLogin );
+        password = (EditText) findViewById( R.id.etPasswordLogin );
+
+        auth = FirebaseAuth.getInstance();
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference("Usuarios");
     }
 }
