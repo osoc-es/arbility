0ef22c1e4229e0b39d7d93f6f89e3dc3e9f7d111
Sergio Gavilan <ssjsrgx@gmail.com>
2019-07-22 11:49:26 +0200

    Map goto Evaluation

--- app/src/main/java/com/osoc/oncera/LoginAlumnoActivity.java
+++ app/src/main/java/com/osoc/oncera/LoginAlumnoActivity.java
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 
 import android.widget.Button;
@@ -49,6 +50,8 @@ public class LoginAlumnoActivity extends AppCompatActivity {
         setContentView( R.layout.activity_login_alumno );
 
         etCodigoItinerario = (EditText) findViewById( R.id.etCodigoItinerario );
+
+        etCodigoItinerario.setInputType(InputType.TYPE_CLASS_TEXT);
 
         btnItineratio = (Button) findViewById( R.id.btnItinerario );
         atras = (ImageButton) findViewById(R.id.btnAtras);
--- app/src/main/java/com/osoc/oncera/MapaItinerarioActivity.java
+++ app/src/main/java/com/osoc/oncera/MapaItinerarioActivity.java
@@ -1,10 +1,12 @@
 package com.osoc.oncera;
 
 
+import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
@@ -28,6 +30,15 @@ import com.mapbox.mapboxsdk.style.layers.Property;
 import com.mapbox.mapboxsdk.style.layers.PropertyFactory;
 import com.mapbox.mapboxsdk.style.layers.SymbolLayer;
 import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
+import com.osoc.oncera.javabean.Ascensores;
+import com.osoc.oncera.javabean.EvacuacionEmergencia;
+import com.osoc.oncera.javabean.Iluminacion;
+import com.osoc.oncera.javabean.Itinerario;
+import com.osoc.oncera.javabean.Obstaculo;
+import com.osoc.oncera.javabean.Puerta;
+import com.osoc.oncera.javabean.PuntosAtencion;
+import com.osoc.oncera.javabean.Rampas;
+import com.osoc.oncera.javabean.SalvaEscaleras;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -50,16 +61,39 @@ public class MapaItinerarioActivity extends AppCompatActivity implements OnMapRe
     ArrayList<Feature> col = new ArrayList<>();
     private GeoJsonSource geoJsonSource;
 
-    private Double[] latitudeList = new Double[] {40.347092, 40.346958, 40.346868, 40.347545};
-    private Double[] longitudeList = new Double[] {-3.696092, -3.696356, -3.696554, -3.696323};
+   // private Double[] latitudeList = new Double[] {40.347092, 40.346958, 40.346868, 40.347545};
+    //private Double[] longitudeList = new Double[] {-3.696092, -3.696356, -3.696554, -3.696323};
 
     private LocationListener locationListener;
     private LocationManager locationManager;
 
-    protected void onCreate(Bundle savedInstanceState) {
+    private Itinerario itinerario;
+
+    private void createItinerario(){
+        ArrayList<Obstaculo> aux = new ArrayList<>();
+
+        Obstaculo p = new Obstaculo("1",-3.696541,40.347230,TypesManager.PUERTAS_OBS,"foto",1,null);
+
+        Obstaculo s = new Obstaculo("2",-3.696155,40.347250,TypesManager.EMERGENC_OBS,"foto",2,null);
+
+        Obstaculo t = new Obstaculo("3",-3.696321,40.346862,TypesManager.ILUM_OBS,"foto",3,null);
+
+        aux.add(p);
+        aux.add(s);
+        aux.add(t);
+
+        itinerario = new Itinerario("1",aux,"Pedro","ItinerarioTest","prueba it",null);
+
+
+    }
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         Mapbox.getInstance(this, "pk.eyJ1IjoiZm9uY2UiLCJhIjoiY2p4b3B1NG53MDhsbTNjbnYzMXNpbjRjYiJ9.MkBM2G0smC9aOJ_IS804xg");
         setContentView(R.layout.activity_mapa_itinerario);
+
+        createItinerario();
+
         mapView = findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
         mapView.getMapAsync(this);
@@ -67,16 +101,45 @@ public class MapaItinerarioActivity extends AppCompatActivity implements OnMapRe
         btn_evaluar = (Button) findViewById(R.id.btn_evaluar);
         sp_obstaculo = (Spinner) findViewById(R.id.sp_obstaculo);
 
-        String[] list = new String[longitudeList.length];
-        for(int i = 0 ; i < longitudeList.length ; i++)
-            list[i] = "Obstáculo: "+Integer.toString(i+1);
+        String[] list = new String[itinerario.getObstaculos().size()];
+        /*for(int i = 0 ; i < longitudeList.length ; i++)
+            list[i] = "Obstáculo: "+Integer.toString(i+1);*/
+
+        for(int i = 0 ; i < itinerario.getObstaculos().size() ; i++){
+            list[i]= "Obstaculo: "+Integer.toString(i+1);
+        }
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_spinner_dropdown_item,list);
 
         sp_obstaculo.setAdapter(adapter);
 
+        btn_evaluar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToEv();
+            }
+        });
+
         Button btn_location = (Button) findViewById(R.id.btn_evaluar);
+    }
+
+    private void goToEv()
+    {
+        Intent i;
+        String t = itinerario.getObstaculos().get((int)sp_obstaculo.getSelectedItemId()).getTipo();
+        if (t == TypesManager.PUERTAS_OBS)  i = new Intent(this,MeasureActivity.class);
+        else if (t == TypesManager.ILUM_OBS)i = new Intent(this,Luxometro.class);
+        else if (t == TypesManager.ASCENSOR_OBS) i = new Intent(this,MedirAscensor.class);
+        else if (t == TypesManager.MOSTRADOR_OBS) i = new Intent(this,MedirMostradorActivity.class);
+        else if (t == TypesManager.RAMPA_OBS) i = new Intent(this,MedirRampa.class);
+        else if (t == TypesManager.SALVAESC_OBS) i = new Intent(this,SalvaEscaleras.class);
+        else if (t == TypesManager.EMERGENC_OBS) i = new Intent(this,MedirEmergencias.class);
+
+        else i = new Intent(this,null);
+
+        startActivity(i);
+
     }
 
     @Override
@@ -88,8 +151,8 @@ public class MapaItinerarioActivity extends AppCompatActivity implements OnMapRe
         latitud = bu.getDouble( "latitud" );
         bu.clear();*/
 
-        LatLng BOUND_CORNER_NW = new LatLng(latitudeList[0]+0.002, longitudeList[0]+0.002);
-        LatLng BOUND_CORNER_SE = new LatLng(latitudeList[0]-0.002, longitudeList[0]-0.002);
+        LatLng BOUND_CORNER_NW = new LatLng(itinerario.getObstaculos().get(0).getLatitud()+0.002, itinerario.getObstaculos().get(0).getLongtiud()+0.002);
+        LatLng BOUND_CORNER_SE = new LatLng(itinerario.getObstaculos().get(0).getLatitud()-0.002, itinerario.getObstaculos().get(0).getLongtiud()-0.002);
         LatLngBounds RESTRICTED_BOUNDS_AREA = new LatLngBounds.Builder()
                 .include(BOUND_CORNER_NW)
                 .include(BOUND_CORNER_SE)
@@ -99,7 +162,7 @@ public class MapaItinerarioActivity extends AppCompatActivity implements OnMapRe
 
 
         mapboxMap.setCameraPosition(new CameraPosition.Builder()
-                .target(new LatLng(latitudeList[0], longitudeList[0]))
+                .target(new LatLng(itinerario.getObstaculos().get(0).getLatitud(), itinerario.getObstaculos().get(0).getLongtiud()))
                 .zoom(16)
                 .build());
 
@@ -112,8 +175,8 @@ public class MapaItinerarioActivity extends AppCompatActivity implements OnMapRe
                 style.addImage(ICON_ID, BitmapFactory.decodeResource(
                         MapaItinerarioActivity.this.getResources(), R.drawable.map_marker));
 
-                for(int i = 0; i<longitudeList.length; i++) {
-                    col.add(Feature.fromGeometry(Point.fromLngLat(longitudeList[i], latitudeList[i])));
+                for(int i = 0; i<itinerario.getObstaculos().size(); i++) {
+                    col.add(Feature.fromGeometry(Point.fromLngLat(itinerario.getObstaculos().get(i).getLongtiud(), itinerario.getObstaculos().get(i).getLatitud())));
                     col.get(col.size()-1).addStringProperty("poi", ICON_ID);
                     col.get(col.size()-1).addStringProperty("title", Integer.toString(i+1));
                 }
